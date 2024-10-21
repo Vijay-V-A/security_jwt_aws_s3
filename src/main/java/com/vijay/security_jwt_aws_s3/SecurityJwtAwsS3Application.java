12531d9b7f92a2586a54886615b94a832f22c123
V A Vijay <vijayva@altimetrik.com>
2024-10-21 16:38:36 +0530

    first commit

---
+++ src/main/java/com/vijay/security_jwt_aws_s3/SecurityJwtAwsS3Application.java
@@ -0,0 +1,13 @@
+package com.vijay.security_jwt_aws_s3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityJwtAwsS3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityJwtAwsS3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/vijay/security_jwt_aws_s3/SecurityJwtAwsS3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vijay.security_jwt_aws_s3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityJwtAwsS3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
